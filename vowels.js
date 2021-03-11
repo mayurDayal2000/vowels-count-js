f4813ef163406ddc9b03b132b745c9290629551a
mayur <mayur.dayal97@gmail.com>
2021-03-11 14:51:59 +0530

    added ui with ES6 code

--- vowels.js
+++ vowels.js
@@ -1,3 +1,5 @@
+/* previous code
+
 function vowelsCounter(strInput){
     let counter = 0;
     vowels ="aeiou";
@@ -9,3 +11,30 @@ function vowelsCounter(strInput){
     return counter;
   }
   console.log(vowelsCounter("rohan singh"))
+*/
+
+const vowelsCounter = (strInput) => {
+  let counter = 0;
+  let vowels = "aeiou";
+
+  for (let i = 0; i <= strInput.length; i++) {
+    if (vowels.includes(strInput[i])) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+const form = document.querySelector("form");
+const msg = document.querySelector(".msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputText = form.vowel.value;
+  let vowelCheck = vowelsCounter(inputText);
+  let template = `<p>Total vowel in the word <strong>${inputText}</strong> : ${vowelCheck}`;
+  msg.innerHTML = template;
+
+  form.reset();
+});
